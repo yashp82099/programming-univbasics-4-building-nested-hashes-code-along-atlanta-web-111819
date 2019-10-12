3aa281abc14bc212b6760bb50dbf61400d533a8f
yash patel <yash82099@yahoo.com>
2019-10-12 01:24:13 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,6 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {name: "Steven", age: '30', attitude: "comfused"}
         
         
       ]
@@ -17,7 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        
+                {name: "Steven", age: '30', attitude: "comfused"}
+
         {name: "Nurse", age: '44', attitude: 'worried'}
       ]
    }
